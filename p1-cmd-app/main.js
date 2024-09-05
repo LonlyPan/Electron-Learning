54d54a3c883587ee6bf44de154a8c1658e3c37e0
LonlyPan <530354836@qq.com>
2024-09-05 17:53:49 +0800

    updata

---
+++ p1-cmd-app/main.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2024 by Lonly, All Rights Reserved. 
+ * @file: main.js
+ * @Author: Lonly
+ * @Since: 2024-09-05 17:24:15
+ * @Description: Header file of xxx module   // 用于详细说明此程序文件完成的主要功能，与其他模块  npm
+ *                                           // 或函数的接口，输出值、取值范围、含义及参数间的控 
+ *                                           // 制、顺序、独立或依赖等关系
+ * @attention:             //注意事项
+ * ------------------------------------------------
+ * @version: v0.1
+ * @LastAuthor: Lonly
+ * @LastEditTime: 2024-09-05 17:24:15
+ * @Modification: 本次版本修改添加了xxxx
+ *  
+ * @History:        // 修改历史记录表
+ *   1.Version: 历史版本号
+ *       Author: 
+ *       date: 历史版本日期
+ *       Modification: 历史版本修改内容
+ *   2.Version: 历史版本号
+ *       ...
+ */
+console.log('Hello from Electron 👋')
